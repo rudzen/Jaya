39088acd3f79d275ee77f5e9851054c943b888cf
Splitwirez <Splitwirez@gmail.com>
2022-08-23 23:00:55 -0400

    Started style/colour cleanup

--- src/Jaya.Shell/App.axaml.cs
+++ src/Jaya.Shell/App.axaml.cs
@@ -1,6 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Avalonia.Markup.Xaml.Styling;
+using Avalonia.Themes.Fluent;
 using Jaya.Shell.ViewModels;
 using Jaya.Shell.Views;
 
@@ -8,9 +13,31 @@ namespace Jaya.Shell
 {
     public class App : Application
     {
+        const string DSGN_THEME = "--designer-theme=";
         public override void Initialize()
         {
             AvaloniaXamlLoader.Load(this);
+
+            string[] args = Environment.GetCommandLineArgs();
+            string dsgnThemeArg = args.FirstOrDefault(x => x.StartsWith(DSGN_THEME));
+            if (!(string.IsNullOrEmpty(dsgnThemeArg) || string.IsNullOrWhiteSpace(dsgnThemeArg)))
+            {
+                string themeName = dsgnThemeArg.Substring(DSGN_THEME.Length);
+                if (themeName == "Light")
+                {
+                    (Styles[0] as FluentTheme).Mode = FluentThemeMode.Light;
+                    var include = Styles[2] as StyleInclude;
+                    Styles.Remove(include);
+                    include = new StyleInclude(new Uri("avares://Jaya.Shell"))
+                    {
+                        Source = new Uri(
+                                $"avares://Jaya.Shell/Styles/Fluent/ColorSchemes/Fluent{themeName}.axaml" //include.Source.AbsolutePath.Replace("Dark", themeName)
+                                //, UriKind.RelativeOrAbsolute)
+                            )
+                    };
+                    Styles.Insert(2, include);
+                }
+            }
         }
 
         public override void OnFrameworkInitializationCompleted()
